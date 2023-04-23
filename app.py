342eb27ca16ad313279302d29256d1ec32e98a78
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-23 15:00:09 +0700

    Install

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# นำเข้า handler ที่เขียนเอง
+from handlers import handle_text_message
+
+app = Flask(__name__)
+
+# ตั้งค่า LINE Bot
+line_bot_api = LineBotApi("YOUR_CHANNEL_ACCESS_TOKEN")
+handler = WebhookHandler("YOUR_CHANNEL_SECRET")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+# สร้าง handler สำหรับข้อความที่เป็น TextMessage
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    handle_text_message(event)
+
+if __name__ == "__main__":
+    app.run()
---
+++ handlers.py
@@ -0,0 +1,23 @@
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from .mongodb import collection
+from .app import line_bot_api
+
+def handle_text_message(event):
+    user_id = event.source.user_id
+    text = event.message.text
+
+    if text.lower() == "เครดิต":
+        user_credit = collection.find_one({"user_id": user_id})
+        if not user_credit:
+            user_credit = {"user_id": user_id, "credits": 0}
+            collection.insert_one(user_credit)
+        
+        reply_text = f"เครดิตของคุณคือ: {user_credit['credits']}"
+
+    else:
+        reply_text = "ขออภัย ฉันไม่เข้าใจคำสั่ง"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
