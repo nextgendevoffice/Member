6797b47cc802c562f46fd3090b41639cc230f26c
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-23 19:37:53 +0700

    Update line_bot.py

--- line_bot/line_bot.py
+++ line_bot/line_bot.py
@@ -40,25 +40,26 @@ def handle_message(event):
             TextSendMessage(text=f"Your current credit is: {credit}")
             )
     elif command == "/withdraw":
-        amount = parse_amount(text)
-        if amount:
-            current_credit = mongodb.get_credit(user_id)
-            if current_credit >= amount:
-                request_id = mongodb.create_withdrawal_request(user_id, amount)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=f"Withdrawal request created. Your request ID is: {request_id}")
-                )
-            else:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text="Insufficient credit or invalid amount.")
-                )
+    amount = parse_amount(text)
+    if amount:
+        success, new_credit = mongodb.withdraw_credit(user_id, amount)  # This line was missing in your code
+        if success:
+            request_id = mongodb.create_withdrawal_request(user_id, amount)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f"Withdrawal request created. Your request ID is: {request_id}")
+            )
         else:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="Invalid command format. Example: /withdraw 50")
+                TextSendMessage(text="Insufficient credit or invalid amount.")
             )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Invalid command format. Example: /withdraw 50")
+        )
+
     elif command == "/withdrawhistory":
         withdrawal_requests = mongodb.get_user_withdrawal_requests(user_id)
         withdrawal_history = "\n".join([f"Request ID: {req['request_id']} | Amount: {req['amount']} | Status: {req['status']}" for req in withdrawal_requests])
